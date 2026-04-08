cfbdee8ef14e00530b3023e36f33568816d9ccc4
coled <deruitercole@gmail.com>
2026-04-08 03:40:08 -0400

    Hide search page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,11 @@
-import type { ReactNode } from "react";
 import type { PropsWithChildren } from "react";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: PropsWithChildren) {
     return (
         <div className="app-container">
-            <Navbar links={['CyberTCG', 'About', 'Decks','Discover', 'Search']}/>
+            <Navbar links={['CyberTCG', 'About', 'Decks','Discover', /*'Search'*/]}/>  {/* hiding search for now */}
             <main className="content">{children}</main>
             <Footer />
         </div>
